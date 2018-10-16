092d218882b70f13bc07febeed1d6adec988889a
Achyudh Ram <achyudhk@gmail.com>
2018-10-16 19:15:16 -0400

    Add code from salesforce/awd-lstm-lm

---
+++ weight_drop_lstm.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+
+from embed_regularize import embedded_dropout
+from locked_dropout import LockedDropout
+from weight_drop import WeightDrop
+
+
+class WeightDropLSTM(nn.Module):
+    """Container module with an encoder, a recurrent module, and a decoder."""
+
+    def __init__(self, ntoken, ninp, nhid, nlayers, dropout=0.5, dropouth=0.5, dropouti=0.5, dropoute=0.1, wdrop=0, tie_weights=False):
+        super(WeightDropLSTM, self).__init__()
+        self.lockdrop = LockedDropout()
+        self.idrop = nn.Dropout(dropouti)
+        self.hdrop = nn.Dropout(dropouth)
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.rnns = [torch.nn.LSTM(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), 1, dropout=0, batch_first=True) for l in range(nlayers)]
+        if wdrop:
+            self.rnns = [WeightDrop(rnn, ['weight_hh_l0'], dropout=wdrop) for rnn in self.rnns]
+        print(self.rnns)
+        self.rnns = torch.nn.ModuleList(self.rnns)
+        self.decoder = nn.Linear(nhid, ntoken)
+        if tie_weights:
+            # Optionally tie weights
+            self.decoder.weight = self.encoder.weight
+
+        self.init_weights()
+
+        self.ninp = ninp
+        self.nhid = nhid
+        self.nlayers = nlayers
+        self.dropout = dropout
+        self.dropouti = dropouti
+        self.dropouth = dropouth
+        self.dropoute = dropoute
+        self.tie_weights = tie_weights
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters()).data
+        return [(weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_(),
+                weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_())
+                for l in range(self.nlayers)]
+
+    def forward(self, input, hidden, return_h=False):
+        emb = embedded_dropout(self.encoder, input, dropout=self.dropoute if self.training else 0)
+        emb = self.lockdrop(emb, self.dropouti)
+
+        raw_output = emb
+        new_hidden = []
+        raw_outputs = []
+        outputs = []
+        for l, rnn in enumerate(self.rnns):
+            current_input = raw_output
+            raw_output, new_h = rnn(raw_output, hidden[l])
+            new_hidden.append(new_h)
+            raw_outputs.append(raw_output)
+            if l != self.nlayers - 1:
+                raw_output = self.lockdrop(raw_output, self.dropouth)
+                outputs.append(raw_output)
+        hidden = new_hidden
+
+        output = self.lockdrop(raw_output, self.dropout)
+        outputs.append(output)
+
+        result = output.view(output.size(0)*output.size(1), output.size(2))
+        if return_h:
+            return result, hidden, raw_outputs, outputs
+        return result, hidden
