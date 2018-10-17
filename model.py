Fix incorrect dimension error in model.py

model.py
@@ -34,7 +34,7 @@ class WDropModel(nn.Module):
         if lengths is not None:
             x = torch.nn.utils.rnn.pack_padded_sequence(x, lengths, batch_first=True)
         self.hidden = WDropModel.repackage_hidden(self.hidden)
-        output, self.hidden, rnn_hs, dropped_rnn_hs = self.wdrop_lstm(x, self.hidden, return_h=True)
+        x, self.hidden = self.wdrop_lstm(x, self.hidden)
         if lengths is not None:
             x, _ = torch.nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
         x = F.relu(torch.transpose(x, 1, 2))

weight_drop_lstm.py
@@ -58,7 +58,6 @@ class WeightDropLSTM(nn.Module):
         raw_outputs = []
         outputs = []
         for l, rnn in enumerate(self.rnns):
-            current_input = raw_output
             raw_output, new_h = rnn(raw_output, hidden[l])
             new_hidden.append(new_h)
             raw_outputs.append(raw_output)
@@ -70,7 +69,7 @@ class WeightDropLSTM(nn.Module):
         output = self.lockdrop(raw_output, self.dropout)
         outputs.append(output)
 
-        result = output.view(output.size(0)*output.size(1), output.size(2))
+        result = output
         if return_h:
             return result, hidden, raw_outputs, outputs
         return result, hidden